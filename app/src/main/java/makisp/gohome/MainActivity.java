73b14109f6826381a4be4e946681adeb8ec6e693
Ευάγγελος Πετρόπουλος <vaggelispetropoulos@live.com>
2016-11-03 23:01:13 +0200

    Έκδοση 0.2.0 Δημιουργία βάσης για τους χρήστες

---
+++ app/src/main/java/makisp/gohome/MainActivity.java
@@ -0,0 +1,16 @@
+package makisp.gohome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    public static DbCredentials db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        db = new DbCredentials(this);
+        db.getWritableDatabase();
+    }
+}
