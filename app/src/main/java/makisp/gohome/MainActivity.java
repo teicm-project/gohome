644d876aa92aefa63fbcb9049af6abfa8c321792
Ευάγγελος Πετρόπουλος <vaggelispetropoulos@live.com>
2016-11-07 23:11:30 +0200

    dawda

--- app/src/main/java/makisp/gohome/MainActivity.java
+++ app/src/main/java/makisp/gohome/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     public static DbCredentials db;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
