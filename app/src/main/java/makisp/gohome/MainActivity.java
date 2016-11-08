b64ce441af2b16e8ad3452af2b92e447e0ad25d1
Ευάγγελος Πετρόπουλος <vaggelispetropoulos@live.com>
2016-11-08 10:03:26 +0200

    hjj

--- app/src/main/java/makisp/gohome/MainActivity.java
+++ app/src/main/java/makisp/gohome/MainActivity.java
@@ -2,12 +2,23 @@ package makisp.gohome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    int p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("log","Sting");
+        Log.d("log","Sting");
+        Log.d("log","Sting");
+        Log.d("log","Sting");
+        Log.d("log","Sting");
+
+
+
+        Log.d("log","Sting");
     }
 }
