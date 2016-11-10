84d3c463caed15b5ea7aada2699280656df5f07f
Stam Dach <stamatis_10@windowslive.com>
2016-11-10 11:47:59 +0200

    Εκδοση καππα

--- app/src/main/java/makisp/gohome/MainActivity.java
+++ app/src/main/java/makisp/gohome/MainActivity.java
@@ -2,12 +2,43 @@ package makisp.gohome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public Button buttonAbout;
+    public static DbCredentials db;
+    public static DbInventory db2;
 
+    public void init() {
+
+        buttonAbout = (Button) findViewById(R.id.buttonAbout);
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new DbCredentials(this);
+        db2 = new DbInventory(this);
+        db.getWritableDatabase();
+        db2.getWritableDatabase();
+
+        Button buttonStart = (Button) findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                    }
+                }
+        );
+        init();
     }
 }
